797791345ab6f37a962868aa9224bb6b61825ca1
SooYeong <76829114+ParkSooYeong@users.noreply.github.com>
2021-07-13 08:52:19 +0900

    Add files via upload

---
+++ MATLAB_1_2_2_1.m
@@ -0,0 +1,16 @@
+% SKU CoE ITE - ParkSooYoung
+% Grade 3 , Semester 1 , Chapter 1-2-2 , Number 1
+
+C = [4/3 1.2345e-6]
+
+format short
+C = [4/3 1.2345e-6]
+
+format long
+C = [4/3 1.2345e-6]
+
+format short e
+C = [4/3 1.2345e-6]
+
+format long e
+C = [4/3 1.2345e-6]
