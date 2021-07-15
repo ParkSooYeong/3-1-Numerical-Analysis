073a0dffd144283f02c6794bfea2c501a92978c3
SooYeong <76829114+ParkSooYeong@users.noreply.github.com>
2021-07-15 16:10:31 +0900

    Add files via upload

---
+++ Chapter 1/MATLAB_1_3_2_1.m
@@ -0,0 +1,11 @@
+% SKU CoE ITE - ParkSooYoung
+% Grade 3 , Semester 1 , Chapter 1-3-2 , Number 1
+
+t = 0:pi/20:15*pi;
+x = cos(t);
+y = sin(t);
+plot3(x,y,t);
+xlabel('x');
+ylabel('y');
+zlabel('z');
+grid;
---
+++ Chapter 1/MATLAB_1_3_2_4.m
@@ -0,0 +1,9 @@
+% SKU CoE ITE - ParkSooYoung
+% Grade 3 , Semester 1 , Chapter 1-3-2 , Number 4
+
+[x, y] = meshgrid(-4:0.2:4);
+z = x.^2-2*y.^2;
+meshz(x, y, z);
+xlabel('x');
+ylabel('y');
+zlabel('z');
