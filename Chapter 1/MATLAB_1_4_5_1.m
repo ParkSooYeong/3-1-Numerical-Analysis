5c1f81482253b30f181461739be99b60e9ec362a
SooYeong <76829114+ParkSooYeong@users.noreply.github.com>
2021-07-22 08:59:07 +0900

    Add files via upload

---
+++ Chapter 1/MATLAB_1_4_5_1.m
@@ -0,0 +1,6 @@
+% SKU CoE ITE - ParkSooYoung
+% Grade 3 , Semester 1 , Chapter 1-4-5 , Number 1
+
+for k = 5:10:35
+    x = 3 * k - 2
+end
---
+++ Chapter 1/MATLAB_1_4_5_2.m
@@ -0,0 +1,8 @@
+% SKU CoE ITE - ParkSooYoung
+% Grade 3 , Semester 1 , Chapter 1-4-5 , Number 2
+
+x = 3;
+while x < 100
+    disp(x)
+    x = 3 * x - 2;
+end
