3a29d133717d1efcd0537129344728e595a915c0
SooYeong <76829114+ParkSooYeong@users.noreply.github.com>
2021-07-23 00:56:19 +0900

    Add files via upload

---
+++ Chapter 1/MATLAB_1_4_6_1.m
@@ -0,0 +1,5 @@
+% SKU CoE ITE - ParkSooYoung
+% Grade 3 , Semester 1 , Chapter 1-4-6 , Number 1
+
+function c = udf1(f)
+c = (f - 32) * 5 / 9;
---
+++ Chapter 1/MATLAB_1_4_6_2.m
@@ -0,0 +1,7 @@
+% SKU CoE ITE - ParkSooYoung
+% Grade 3 , Semester 1 , Chapter 1-4-6 , Number 2
+
+function [S,V] = udf2(r)
+    S = 4 * pi * r.^2;
+    V = pi * r.^3 * (4 / 3);
+ 
