02966b77fcc5b8894a9ff91ef3b412582ddef815
SooYeong <76829114+ParkSooYeong@users.noreply.github.com>
2021-07-29 01:27:40 +0900

    Add files via upload

---
+++ Chapter 2/MATLAB_2_3_1_1.m
@@ -0,0 +1,7 @@
+% SKU CoE ITE - 20170910 ParkSooYoung
+% Grade 3 , Semester 1 , Chapter 2-3-1 , Number 1
+
+x = [0.5,1,1.5,2.5,3,4];
+y = [110,270,350,260,200,150];
+x_int = [2,3,5];
+interp1(x,y,x_int)
