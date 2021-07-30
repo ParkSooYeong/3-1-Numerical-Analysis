dbb94b44d67ebdb86f8818fd014414bcfd5668d9
SooYeong <76829114+ParkSooYeong@users.noreply.github.com>
2021-07-31 01:06:52 +0900

    Add files via upload

---
+++ Chapter 2/MATLAB_2_4_1_1.m
@@ -0,0 +1,14 @@
+% SKU CoE ITE - 20170910 ParkSooYoung
+% Grade 3 , Semester 1 , Chapter 2-4-1 , Number 1
+
+x = linspace(0,pi,10);
+y = sin(x);
+trapz(x,y)
+
+x = linspace(0,pi,150);
+y = sin(x);
+trapz(x,y)
+
+x = linspace(0,pi,183);
+y = sin(x);
+trapz(x,y)
