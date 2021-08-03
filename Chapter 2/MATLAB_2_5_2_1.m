df08bc3957e6d72e9598dde2a83a6ff8c5170a9b
SooYeong <76829114+ParkSooYeong@users.noreply.github.com>
2021-08-04 00:10:21 +0900

    Add files via upload

---
+++ Chapter 2/MATLAB_2_5_2_1.m
@@ -0,0 +1,8 @@
+% SKU CoE ITE - 20170910 ParkSooYoung
+% Grade 3 , Semester 1 , Chapter 2-5-2 , Number 1
+
+dsolve('Dy=cos(t)','y(0)=1')
+
+[y,z] = dsolve('Dy=4*y+3*z','Dz=3*y+4*z','y(0)=0','z(0)=1')
+
+dsolve('D2z=d^2*z','z(0)=1','Dz(0)=0')
