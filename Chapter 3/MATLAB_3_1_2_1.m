6cc4d2bcf32badbfbe150116f3031c34a177e9c6
SooYeong <76829114+ParkSooYeong@users.noreply.github.com>
2021-08-06 00:19:39 +0900

    Add files via upload

---
+++ Chapter 3/MATLAB_3_1_2_1.m
@@ -0,0 +1,9 @@
+% SKU CoE ITE - 20170910 ParkSooYoung
+% Grade 3 , Semester 1 , Chapter 3-1-2 , Number 1
+
+format long
+
+Fr_bin2dec('0.01010101010101')
+Fr_bin2dec(0.01010101010101)
+Fr_dec2bin('0.2')
+Fr_dec2bin(0.2)
