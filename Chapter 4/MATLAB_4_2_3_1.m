028c2777a4b12b0c82c567b8368bdc90a027e955
SooYeong <76829114+ParkSooYeong@users.noreply.github.com>
2021-08-13 00:37:26 +0900

    Add files via upload

---
+++ Chapter 4/MATLAB_4_2_3_1.m
@@ -0,0 +1,10 @@
+% SKU CoE ITE - 20170910 ParkSooYoung
+% Grade 3 , Semester 1 , Chapter 4-2-3 , Number 1
+
+A = [-1 3 4; -1 1 2; 3 -1 1];
+det(A)
+inv(A)
+
+B = [1 5 3; 2 8 6; 1 2 3];
+det(B)
+inv(B)
---
+++ Chapter 4/MATLAB_4_2_3_2.m
@@ -0,0 +1,6 @@
+% SKU CoE ITE - 20170910 ParkSooYoung
+% Grade 3 , Semester 1 , Chapter 4-2-3 , Number 2
+
+A = [-1 3 4; -1 1 2; 3 -1 1];
+c = [4; 2; 1];
+x = A \ c
