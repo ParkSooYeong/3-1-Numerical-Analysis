c9f3509ac7566d7e53b119a571e5a6c4995e635c
SooYeong <76829114+ParkSooYeong@users.noreply.github.com>
2021-08-21 02:05:15 +0900

    Rename F_2_6.m to Report/2021-05-03/F_2_6.m

--- F_2_6.m
+++
@@ -1,8 +0,0 @@
-% SKU CoE ITE - 20170910 ParkSooYoung
-% Grade 3 , Semester 1 , Chapter 2 , Number 2.6
-
-% Function 2.6 : 정확한 근을 찾기 위한 사용자정의함수
-
-function y = F_2_6(x)               % 주어진 방정식을 풀기 위한 사용자정의함수 F_2_6 선언
-    y = exp(-0.2.*x).*cos(x+2)+0.1; % 우변의 -0.1을 이항하여 y = 0인 상태
-end                                 % 사용자정의함수 F_2_6 종료
